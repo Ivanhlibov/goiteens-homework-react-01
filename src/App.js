b2ea883aca4b38d3293051b226811db24817f6e2
Ivanhlibov <92669197+Ivanhlibov@users.noreply.github.com>
2023-07-24 16:14:02 +0300

    text

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { Message } from './components/message';
+import './App.css';
+import { Greeting } from './components/greeting';
+
+function App() {
+  return (
+
+
+    <>
+     <Greeting name = "Ivan" ></Greeting>
+     <Message name = " Your text "></Message>
+    </>
+
+  );
+}
+
+export default App;
---
+++ src/components/message.jsx
@@ -0,0 +1,3 @@
+export const Message = ({text})=>{
+    return <h1>text</h1>
+}
